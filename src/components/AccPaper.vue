b193d25e3202db8194a95e6e4b05afcbc4d63621
Dmitriy Pankratov <7snak7@gmail.com>
2024-10-01 00:12:44 +0700

    alternative ui

--- src/components/AccPaper.vue
+++ src/components/AccPaper.vue
@@ -7,45 +7,73 @@
         inputs: ['','','','','','',''],
         col: ['','','','','','',''],
       }
+    },
+    methods:{
+      movefocus: function() {
+
+      }
     }
   }
 </script>
 
 <template>
-  <div class="paper" v-for="i of 2" :key="i">
-    <div class="head">
-      <h3>Акт выполненных работ</h3>
-      <div class="data">"__"____________20__г.</div>
-      <div class="number">№____</div>
-    </div>
+  <div class="container" v-for="m of 3" :key="m">
+    <div class="head">Акт выполненных работ</div>
     <div class="logo"><img alt="Logo" src="../assets/logo.png"></div>
+    <div class="data">"__"____________20__г.</div>
+    <div class="number">№____</div>
+    <div class="shop">Затребовал</div>
+    <div class="list">
+      <div>№</div>
+      <div>Наименование</div>
+      <div>Кол-во</div>
+      <div>Цена</div>
+      <div>Сумма</div>
 
-    <div class="inputUnderline">
-      Затребовал
-      <input class="inputName" type="text" v-model="shop">
+      <div>1</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>2</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>3</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>4</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>5</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>6</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <div>7</div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
-    <table class="list">
-      <tr class="head-list">
-        <td>№</td>
-        <td>Наименование</td>
-        <td>Кол-во</td>
-        <td>Цена</td>
-        <td>Сумма</td>
-      </tr>
-      <tr v-for="n in 5" :key="n">
-        <td>{{ n }}</td>
-        <td><input class="inputName" type="text" v-model="inputs[n-1]"></td>
-        <td><input type="text" class="col" v-model="col[n-1]"></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </table>
-    <div class="inputUnderline signature">
-      Отпустил
-    </div>
-    <div class="inputUnderline signature">
-      Получил
-    </div>
+    <div class="released">Отпустил</div>
+    <div class="received">Получил</div>
   </div>
 </template>
 
@@ -57,6 +85,7 @@
     /* Обнулить поля на странице */
     @page {
       margin: 0;
+      padding: 0;
       size: A4;
     }
     html, body{
@@ -68,94 +97,44 @@
       color-adjust: exact !important;
     }
   }
-  .paper {
-    max-width: 800px;
+
+  .container {
+    width: 140mm;
     margin-left: auto;
     margin-right: auto;
+    display: grid;
+    gap: 3mm;
+    padding: 5mm;
     background-color: white;
     border: 1px solid black;
-    padding: 10pt;
+    grid-template-columns: 60mm 20mm 50mm;
+    grid-template-rows: 3mm 3mm 3mm auto 3mm 3mm;
   }
   .head {
-    text-align: left;
-    float: left;
-    text-transform: uppercase;
-    width: 60%;
-    padding: 10px 30px;
+    grid-column: 1/3;
   }
-  .data {
-    text-align: left;
-    float: left;
-    width: 70%;
+  .logo {
+    grid-row: 1/3;
+    grid-column: 3/4;
   }
-  .number {
-    text-align: right;
-    float: right;
-    width: 30%;
+  .shop, .received, .released {
+    grid-column: 1/4;
+  }
+  .list {
+    grid-column: 1/4;
+    display: grid;
+    grid-template-columns: 8mm auto 18mm 18mm 18mm;
+    border: 1px solid black;
+  }
+  .list div {
+    border: 1px solid black;
   }
   .logo {
     text-align: right;
     float: right;
-    width: 40%;
-    padding: 30px 20px;
   }
   img {
     max-width: 100%;
   }
-  .inputUnderline {
-    text-align: left;
-    width: 100%;
-    padding: 0px 0px;
-    margin: 20px 0px;
-    position: relative;
-  }
-  .inputUnderline input {
-    position: absolute;
-    width: 500px;
-    left: 130px;
-    bottom: 6px;
-  }
-  .inputUnderline::before {
-    content: '';
-    position: absolute;
-    background-color: black;
-    width: 500px;
-    height: 2px;
-    bottom: 4px;
-    left: 130px;
-  }
-  .signature::after {
-    content: '(должность,подпись,расшифровка)';
-    font-size: 10px;
-    position: absolute;
-    left: 250px;
-    top: 18px;
-  }
-  .list {
-    border-collapse: collapse;
-    width: 100%;
-  }
-  .list td {
-    border-style: solid;
-    border-width: 2px;
-    border-color: black;
-    padding: 2pt
-  }
-  .head-list td {
-    padding: 6pt;
-    font-weight: bold;
-    font-size: 12pt;
-  }
-  .inputName {
-    font-size: 12pt;
-    width: 400px;
-    border-style: none;
-    //background-color: lightgray;
-  }
-  .col {
-    font-size: 12pt;
-    width: 90%;
-    border-style: none;
-    //background-color: lightgray;
-  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/theme.css'
+// import './assets/theme.css'
 
 createApp(App).mount('#app')
